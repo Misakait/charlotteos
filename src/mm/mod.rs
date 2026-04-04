ca8e15d30f37c137bab8357aa32d7bf4eb33a29d
misakait <cjl2597588144@gmail.com>
2026-04-04 23:43:38 +0800

    Add boot mapping modes and buddy allocator
    Extend map_segment to support BootMapType (Identical/Linear) and
    MapAction
    (Map/Unmap). Update boot mappings to create both linear and identical
    mappings and enable unmapping of temporary identity mappings.
    
    Implement a locked BUDDY_ALLOCATOR and init_buddy_system to transfer
    Memblock regions into the buddy allocator. Add enable_virtual_memory to
    write SATP and perform an SFENCE, and add unmap_temp_identity_area

--- src/mm/mod.rs
+++ src/mm/mod.rs
@@ -16,13 +16,17 @@ use crate::mm::pagetable::{FrameTracker, PTEFlags, PageSize, PageTable};
 use crate::{polling_println, println};
 use buddy::BuddySystemFrameAllocator;
 use core::alloc::{GlobalAlloc, Layout};
+use core::arch::asm;
+use core::arch::riscv64::sfence_vma_all;
 use core::cell::RefCell;
 use core::num::NonZeroUsize;
 use core::ptr;
 use core::ptr::NonNull;
 use fdt::Fdt;
 use lazy_static::lazy_static;
+use riscv::register::satp::{self, Mode, Satp};
 use spin::Mutex;
+use spin::mutex::SpinMutex;
 pub const PAGE_SIZE_BITS: usize = 12;
 pub const PAGE_SIZE: usize = 1 << PAGE_SIZE_BITS; // 4KB
 
@@ -142,14 +146,65 @@ pub fn setup_memory_and_mapping(dtb_addr: usize) {
         }
     }
 
-    // 内核精细化映射
-    map_segment(stext, etext, PTEFlags::R | PTEFlags::X, root_pt);
+    map_segment(
+        stext,
+        etext,
+        root_pt,
+        BootMapType::Linear,
+        MapAction::Map(PTEFlags::R | PTEFlags::X),
+    );
+    map_segment(
+        stext,
+        etext,
+        root_pt,
+        BootMapType::Identical,
+        MapAction::Map(PTEFlags::R | PTEFlags::X),
+    );
     polling_println!("Mapped text -> {:#x}..{:#x}", stext, etext);
-    map_segment(srodata, erodata, PTEFlags::R, root_pt);
+    map_segment(
+        srodata,
+        erodata,
+        root_pt,
+        BootMapType::Linear,
+        MapAction::Map(PTEFlags::R),
+    );
+    map_segment(
+        srodata,
+        erodata,
+        root_pt,
+        BootMapType::Identical,
+        MapAction::Map(PTEFlags::R),
+    );
     polling_println!("Mapped rodata -> {:#x}..{:#x}", srodata, erodata);
-    map_segment(sdata, edata, PTEFlags::R | PTEFlags::W, root_pt);
+    map_segment(
+        sdata,
+        edata,
+        root_pt,
+        BootMapType::Linear,
+        MapAction::Map(PTEFlags::R | PTEFlags::W),
+    );
+    map_segment(
+        sdata,
+        edata,
+        root_pt,
+        BootMapType::Identical,
+        MapAction::Map(PTEFlags::R | PTEFlags::W),
+    );
     polling_println!("Mapped data -> {:#x}..{:#x}", sdata, edata);
-    map_segment(sbss_with_stack, ebss, PTEFlags::R | PTEFlags::W, root_pt);
+    map_segment(
+        sbss_with_stack,
+        ebss,
+        root_pt,
+        BootMapType::Linear,
+        MapAction::Map(PTEFlags::R | PTEFlags::W),
+    );
+    map_segment(
+        sbss_with_stack,
+        ebss,
+        root_pt,
+        BootMapType::Identical,
+        MapAction::Map(PTEFlags::R | PTEFlags::W),
+    );
     polling_println!("Mapped bss -> {:#x}..{:#x}", sbss_with_stack, ebss);
 
     // 内核终点到物理内存终点
@@ -157,8 +212,9 @@ pub fn setup_memory_and_mapping(dtb_addr: usize) {
     map_segment(
         ram_start_after_kernel,
         ram_end,
-        PTEFlags::R | PTEFlags::W,
         root_pt,
+        BootMapType::Linear,
+        MapAction::Map(PTEFlags::R | PTEFlags::W),
     );
     polling_println!(
         "Mapped kernel -> {:#x}..{:#x}",
@@ -166,54 +222,66 @@ pub fn setup_memory_and_mapping(dtb_addr: usize) {
         ram_end
     );
 
-    // 6. 将根页表移交给激活阶段
+    //  将根页表移交给激活阶段
     *BOOT_ROOT_PPN.borrow_mut() = PhysPageNum::from(root_pa);
 }
-
-pub fn map_segment(start_addr: usize, end_addr: usize, flags: PTEFlags, root_pt: &mut PageTable) {
+enum BootMapType {
+    Identical,
+    Linear,
+}
+#[derive(Clone, Copy)]
+pub enum MapAction {
+    Map(PTEFlags),
+    Unmap,
+}
+pub fn map_segment(
+    start_addr: usize,
+    end_addr: usize,
+    root_pt: &mut PageTable,
+    map_type: BootMapType,
+    action: MapAction,
+) {
     let page_start = align_down(start_addr, PAGE_SIZE);
     let page_end = align_up(end_addr, PAGE_SIZE);
     let aligned_start = align_up(page_start, 0x20_0000).min(page_end);
     let aligned_end = align_down(page_end, 0x20_0000).max(aligned_start);
 
+    let pa_to_va = match map_type {
+        BootMapType::Identical => |pa: usize| pa,
+        BootMapType::Linear => |pa: usize| phys_to_virt(pa),
+    };
+
+    let mut apply_action = |pa: usize, size: PageSize| {
+        let va = pa_to_va(pa);
+        let vpn = VirtPageNum::from(VirtAddr::from(va));
+
+        match action {
+            MapAction::Map(flags) => {
+                root_pt.bump_map(vpn, PhysPageNum::from(PhysAddr(pa)), flags, size);
+            }
+            MapAction::Unmap => {
+                root_pt.unmap(vpn, size); // 直接复用你上一轮修好的完美 unmap
+            }
+        }
+    };
+
     let head_end = aligned_start.min(page_end);
     let mut current_pa = page_start;
     while current_pa < head_end {
-        let va = current_pa + PHYS_VIRT_OFFSET;
-        let vpn = VirtAddr::from(va);
-        root_pt.bump_map(
-            VirtPageNum::from(vpn),
-            PhysPageNum::from(PhysAddr(current_pa)),
-            flags,
-            PageSize::FourKB,
-        );
+        apply_action(current_pa, PageSize::FourKB);
         current_pa += PAGE_SIZE;
     }
 
     let mut current_pa = aligned_start;
     let mid_end = aligned_end.min(page_end);
     while current_pa < mid_end {
-        let va = current_pa + PHYS_VIRT_OFFSET;
-        let vpn = VirtAddr::from(va);
-        root_pt.bump_map(
-            VirtPageNum::from(vpn),
-            PhysPageNum::from(PhysAddr(current_pa)),
-            flags,
-            PageSize::TwoMB,
-        );
+        apply_action(current_pa, PageSize::TwoMB);
         current_pa += 0x20_0000;
     }
 
     let mut current_pa = mid_end;
     while current_pa < page_end {
-        let va = current_pa + PHYS_VIRT_OFFSET;
-        let vpn = VirtAddr::from(va);
-        root_pt.bump_map(
-            VirtPageNum::from(vpn),
-            PhysPageNum::from(PhysAddr(current_pa)),
-            flags,
-            PageSize::FourKB,
-        );
+        apply_action(current_pa, PageSize::FourKB);
         current_pa += PAGE_SIZE;
     }
 }
@@ -226,4 +294,90 @@ fn align_down(value: usize, align: usize) -> usize {
     value & !(align - 1)
 }
 
-fn init_buddy_system() {}
+type LockedBuddyAllocator = SpinMutex<BuddySystemFrameAllocator>;
+pub static BUDDY_ALLOCATOR: LockedBuddyAllocator = SpinMutex::new(BuddySystemFrameAllocator::new());
+
+pub fn init_buddy_system() {
+    let mb = MEMBLOCK.lock();
+    let mut buddy = BUDDY_ALLOCATOR.lock();
+
+    polling_println!("--- Transferring memory from Memblock to Buddy System ---");
+
+    // 遍历 Memblock 中所有真正可用的物理内存碎片
+    for i in 0..mb.available.count {
+        let region = &mb.available.regions[i];
+
+        let start_pa = region.base.0;
+        let end_pa = region.base.0 + region.size;
+
+        polling_println!(
+            "Transferring region: {:#x}..{:#x} (Size: {} KB)",
+            start_pa,
+            end_pa,
+            region.size / 1024
+        );
+
+        unsafe {
+            buddy.add_free_region(start_pa, end_pa);
+        }
+    }
+    polling_println!("--- Transfer Complete! ---");
+}
+
+pub fn enable_virtual_memory() {
+    let root_ppn = BOOT_ROOT_PPN.borrow().0;
+
+    let mut satp = Satp::from_bits(0);
+    satp.set_mode(Mode::Sv39);
+    satp.set_asid(0);
+    satp.set_ppn(root_ppn);
+    unsafe {
+        satp::write(satp);
+        asm!("sfence.vma");
+    }
+}
+
+pub fn unmap_temp_identity_area() {
+    let stext = unsafe { &_text_start as *const _ as usize };
+    let etext = unsafe { &_text_end as *const _ as usize };
+    let srodata = unsafe { &_rodata_start as *const _ as usize };
+    let erodata = unsafe { &_rodata_end as *const _ as usize };
+    let sdata = unsafe { &_data_start as *const _ as usize };
+    let edata = unsafe { &_data_end as *const _ as usize };
+    let sbss_with_stack = unsafe { &_bss_start_with_stack as *const _ as usize };
+    let ebss = unsafe { &_bss_end as *const _ as usize };
+
+    let ppn: PhysAddr = (&*BOOT_ROOT_PPN.borrow()).into();
+    let root_pa = ppn;
+    let root_pt = unsafe { &mut *(root_pa.0 as *mut PageTable) };
+
+    map_segment(
+        stext,
+        etext,
+        root_pt,
+        BootMapType::Identical,
+        MapAction::Unmap,
+    );
+    map_segment(
+        srodata,
+        erodata,
+        root_pt,
+        BootMapType::Identical,
+        MapAction::Unmap,
+    );
+
+    map_segment(
+        sdata,
+        edata,
+        root_pt,
+        BootMapType::Identical,
+        MapAction::Unmap,
+    );
+    map_segment(
+        sbss_with_stack,
+        ebss,
+        root_pt,
+        BootMapType::Identical,
+        MapAction::Unmap,
+    );
+}
