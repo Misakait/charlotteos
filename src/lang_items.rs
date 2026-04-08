37f5af954af3fcb228b80b16df3d394266a2781d
misakait <cjl2597588144@gmail.com>
2026-04-08 19:11:07 +0800

    Use sbi_println in panic handler

--- src/lang_items.rs
+++ src/lang_items.rs
@@ -1,5 +1,5 @@
 use crate::bsp::qemu_virt::{FINISHER_FAIL, FINISHER_PASS, VIRT_TEST_ADDR};
-use crate::{polling_println, println};
+use crate::{polling_println, println, sbi_println};
 use core::panic::PanicInfo;
 use core::ptr::write_volatile;
 
@@ -7,7 +7,7 @@ const STATUS_CODE: u32 = 7;
 
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    polling_println!("{} with message: {}", info, info.message());
+    sbi_println!("{} with message: {}", info, info.message());
     // let shutdown_cmd: u32 = (STATUS_CODE << 16) | (FINISHER_FAIL as u32);
     // unsafe{
     //     write_volatile(VIRT_TEST_ADDR as *mut u32,shutdown_cmd);
